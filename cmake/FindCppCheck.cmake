e7f0be20be77f11d6cbe9cfc02c0d504e7e1ecb2
Andre Meyering <info@andremeyering.de>
2019-01-29 21:36:47 +0100

    [CMake] Fix formatting

--- cmake/FindCppCheck.cmake
+++ cmake/FindCppCheck.cmake
@@ -1,8 +1,10 @@
-find_program(CPPCHECK_EXE
+find_program(
+  CPPCHECK_EXE
   NAMES cppcheck
   PATHS /usr/bin/
   PATH_SUFFIXES bin
-  DOC "static code analysis tool")
+  DOC "static code analysis tool"
+)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(CppCheck DEFAULT_MSG CPPCHECK_EXE)
