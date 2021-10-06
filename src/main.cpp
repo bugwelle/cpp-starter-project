cecb0789bf7ec4e5531dcef4b18221f8c3954108
Andre Meyering <info@andremeyering.de>
2021-10-06 11:46:56 +0200

    [Project] Apply clang-format diffs

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 
 #include <clara/clara.hpp>
 #include <range/v3/algorithm.hpp>
-#include <range/v3/numeric.hpp>
 #include <range/v3/iterator.hpp>
+#include <range/v3/numeric.hpp>
 
 #include <iostream>
 #include <optional>
