9938cf676deb225fa82bfc61a966667ea7640f20
Andre Meyering <info@andremeyering.de>
2021-11-28 20:03:42 +0100

    [Tools] Explicitly use clang-format 12

--- tools/run_clang_format.sh
+++ tools/run_clang_format.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -Eeuo pipefail
 IFS=$'\n\t'
 
-cd "$( cd "$(dirname "$0")"; pwd -P )/.."
+# Go to project directory
+cd "$(dirname "${BASH_SOURCE[0]}")/.." > /dev/null 2>&1
+
 source tools/utils.sh
 
-print_info "Format all files using clang-format"
-# We currently don't format test files
+# Explicitly use clang-format in version 12.
+# As I use macOS with MacPorts, we search for it as well.
+if [[ -x "$(command -v clang-format-12)" ]]; then
+	CF=clang-format-12
+elif [[ -x "$(command -v clang-format-mp-12)" ]]; then
+	# MacPorts version
+	CF=clang-format-mp-12
+else
+	CF=clang-format
+	clang-format --version | grep " 12." > /dev/null || (print_warning "WARNING: MediaElch requires clang-format version 12")
+fi
+
+print_important "Format all source files using ${CF}"
 find src tests \
-    -type f \( -name "*.cpp" \
-    -o -name "*.hpp" -o -name "*.h" \) \
-    -exec clang-format \
-    -i -style=file {} \;
+    -type f \( -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) \
+    -exec ${CF} \
+    -i -style=file {} \+
+
+print_success "Done"
